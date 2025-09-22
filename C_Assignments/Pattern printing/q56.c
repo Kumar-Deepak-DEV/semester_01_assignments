73c57e91b994e3896e55b9f9a3b030669ad66533
Deepak Kumar <kumar.deepak.cg.s@gmail.com>
2025-09-22 18:47:17 +0530

    resolved mistakes in q56

--- C_Assignments/Pattern printing/q56.c
+++ C_Assignments/Pattern printing/q56.c
@@ -1,17 +1,21 @@
 #include<stdio.h>
 int main(){
-    int num;
-    printf("Enter your number: ");
+
+    int num, min;
     scanf("%d",&num);
+    for (int i=1; i<=num; i++){
+        for (int j=1; j<=num; j++){
+            if (i<j) min=i;
+            else min=j;
+            if((num-i)< min) min=num-i+1; 
+            if((num-j)< min) min=num-j+1;
 
-    for (int i=1; i<=num; i+=1){
-        for(int j=1; j<=num; j+=1){
-            if(i==1 || j==1 || j==num || i==num ) printf("*");
-            else if{
-
-            }
+            if(min%2!=0)
+            printf("*");
+            else
+            printf(" ");
         }
+        printf("\n");
     }
-
     return 0;
 }
